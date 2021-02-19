2c55555a4dec1083e542cdbd344ca2f04894218f
HACKERSPUNK <79210553+Punker-Bhai@users.noreply.github.com>
2021-02-19 09:56:26 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo
+echo -e "\e[1;31mThis Tool Will Install Kali Linux in your Termux"
+echo
+sleep 2.0
+echo -e "\e[0;32mImportant Instructions Before Installing Kali Linux"
+echo
+echo -e "1) Don't keep termux in background"
+echo -e "2) Don't let your display sleep"
+echo -e "3) Good Internet speed"
+echo -e "4) Patience : Will take time"
+sleep 2.0
+echo -e "\e[4;34mInstallation Process Starting"
